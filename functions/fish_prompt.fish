47245489fc284aab1be8ee146470f0a59350b79e
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2018-10-15 18:21:39 +0800

    Support MINIMAL_PROMPT

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,14 +1,19 @@
 function fish_prompt --description 'Prompt'
   set -l last_status $status
 
-  echo ""
+  if test -z "$MINIMAL_PROMPT"
+    echo ""
 
-  __rsc_ssh
-  __rsc_pwd
-  __rsc_git
+    __rsc_ssh
+    __rsc_pwd
+    __rsc_git
 
-  set_color $rsc_color3
-  echo -n ' › '
+    set_color $rsc_color3
+    echo -n ' › '
+  else
+    set_color $rsc_color3
+    echo -n '› '
+  end
 
   set_color normal
 end
